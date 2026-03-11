d98f0085ce0c9bf43a7c7e2d3cb50125f507f8b8
Minahil <minahil.066@outlook.com>
2026-03-11 23:50:08 +0500

    Update secret_handshake.sh

--- Bash_prac/secret_handshake.sh
+++ Bash_prac/secret_handshake.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 n=$1
 bin=()
-actions=()
+action=()
 bin=$(echo "obase=2; $n" | bc )
- while(( ${#bin}<6)); do
+ while(( ${#bin}<5)); do
  bin="0$bin"
  done
  if [[ "${bin:4:1}" == "1" ]]; then 
- echo "wink" 
  action+=("wink") 
  fi
  if [[ "${bin:3:1}" == "1" ]]; then 
- echo "double blink" 
  action+=("double blink") 
  fi
  if [[ "${bin:2:1}" == "1" ]]; then 
- echo "close your eyes" 
- actions +=("close your eyes") 
+ action+=("close your eyes") 
  fi
  if [[ "${bin:1:1}" == "1" ]]; then 
- echo "jump" 
- actions+=("jump")
+ action+=("jump")
+ fi
  if [[ "${bin:0:1}" == "1" ]]; then
     reversed=()
-    for ((i=${#actions[@]}-1; i>=0; i--)); do
-        reversed+=("${actions[i]}")
+    for ((i=${#action[@]}-1; i>=0; i--)); do
+        reversed+=("${action[i]}")
     done
-    actions=("${reversed[@]}")
+    action=("${reversed[@]}")
 fi
-
- 
+len=${#action[@]}
+for ((i=0;i<len; i++)); do
+echo -n "${action[i]}"
+if (( i < len-1)); then
+echo -n ","
+fi
+done
