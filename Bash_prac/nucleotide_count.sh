b8469f5d1b89d787338ca31532589d305a6285f3
Minahil <minahil.066@outlook.com>
2026-03-19 02:51:21 +0500

    Update nucleotide_count.sh

--- Bash_prac/nucleotide_count.sh
+++ Bash_prac/nucleotide_count.sh
@@ -1,1 +1,39 @@
 
+#!/bin/bash
+n="$1"
+len=${#n}
+A=0
+C=0
+G=0
+T=0
+for((i=0;i<len;i++)); do
+if [[ "${n:i:1}" == "A" ]]; then
+((A++))
+elif [[ "${n:i:1}" == "C" ]]; then
+((C++))
+elif [[ "${n:i:1}" == "G" ]]; then
+((G++))
+elif [[ "${n:i:1}" == "T" ]]; then
+((T++))
+else
+echo "Invalid nucleotide in strand"
+exit 1
+fi
+done
+echo "A: $A"
+echo "C: $C"
+echo "G: $G"
+echo "T: $T"
+
+#one-liner version
+
+ #!/bin/ bash
+n="$1"
+if [[ "$n" =~ [^ACGT] ]] && [[ -n "$n" ]]; then
+    echo "Invalid nucleotide in strand"
+    exit 1
+    fi
+for base in A C G T; do
+    count=$(echo "$n" | grep -o "$base" | wc -l)
+    echo "$base: $count"
+done
