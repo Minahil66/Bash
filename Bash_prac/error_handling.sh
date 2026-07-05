068dd24026561732abe02ae45c2ba26680b78b58
Minahil <minahil.066@outlook.com>
2026-07-05 22:00:08 +0500

    Update error_handling.sh

--- Bash_prac/error_handling.sh
+++ Bash_prac/error_handling.sh
@@ -1,1 +1,11 @@
-
+#!/bin/ bash
+if [[ $# -eq 0 ]]; then 
+echo "Usage: error_handling.sh <person>"
+exit 1
+fi
+if [[ $# -eq 1 ]]; then
+echo "Hello, $1"
+exit 0
+fi
+echo "Usage: error_handling.sh <person>"
+exit 1;
